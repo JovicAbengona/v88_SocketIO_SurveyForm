e8894799a57615d889007ae4f6a7dfb5a5608ff9
JovicAbengona <jvlabengona@gmail.com>
2022-05-23 11:57:59 +0800

    Code cleanup and minor changes

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const app = express();
-const server = app.listen(8080, function(){
-    console.log("Listening on 8080");
-});
-const io = require('socket.io')(server);
+const app     = express();
+const server  = app.listen(8080, () => { console.log("Listening on 8080"); });
+const io      = require('socket.io')(server);
+
 app.use(express.static(__dirname + "/static"));
 app.set('views', __dirname + '/views'); 
 app.set('view engine', 'ejs');
@@ -12,17 +11,18 @@ app.get("/", (request, response) => {
     response.render("index");
 });
 
-io.on('connection', (socket) => { //2
+io.on('connection', (socket) => {
     console.log("User connected " + socket.id);
+
     socket.on("posting_form", data => {
-        console.log(data.form_data);
         let random_number = Math.floor(Math.random() * 1000) + 1;
         let message = {
-            main: `You emitted the following information to the server: 
-                    {name: '${data.form_data.name}', dojo_location: '${data.form_data.dojo_location}', fave_language: '${data.form_data.fave_language}', 
-                    comment: '${data.form_data.comment}'}`,
+            main: `You emitted the following information to the server:
+                    { name: '${data.form_data.name}', dojo_location: '${data.form_data.dojo_location}', fave_language: '${data.form_data.fave_language}', 
+                    comment: '${data.form_data.comment}' }`,
             random: `Your lucky number emitted by the server is ${random_number}`
         };
+
         socket.emit("updated_message", {message: message});
     });
 });
